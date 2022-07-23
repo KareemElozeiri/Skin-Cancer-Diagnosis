28b8d3e7319539d43cc92111dbeadfe1ac3ca482
KareemElozeiri <K.elozeiri@gmail.com>
2022-07-23 23:30:02 +0200

    create_model function added

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.create_model import create_model
+
+if __name__ == "__main__":
+    clasifer = create_model()
---
+++ src/create_model.py
@@ -0,0 +1,22 @@
+from keras import layers, models
+
+def create_model():
+    model = models.Sequential()
+    # convolutional layers
+    model.add(layers.Conv2D(32, (3,3), activation="relu"))
+    model.add(layers.MaxPooling2D((2,2)))
+
+    model.add(layers.Conv2D(64, (3,3), activation="relu"))
+    model.add(layers.MaxPooling2D((2,2)))
+
+    model.add(layers.Conv2D(128, (3,3), activation="relu"))
+    model.add(layers.MaxPooling2D((2,2)))
+
+    model.add(layers.Conv2D(128, (3,3), activation="relu"))
+    model.add(layers.MaxPooling2D((2,2)))
+    #dense layers
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation="relu"))
+    model.add(layers.Dense(1, activation="sigmoid"))
+
+    return model 
